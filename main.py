f7505de557ca5a0f4951bc05a98351ab550cfa14
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-02 21:07:59 +0100

    Initial draft of Indeed scraper run on main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.data_processing import DataProcessing
-from src.general_scraper import GeneralScraper
+from src.indeed_scraper import IndeedScraper
 from src.database_operations import DatabaseOperations
+
+indeed_instance = IndeedScraper("https://uk.indeed.com/", 'indeed_config.json', 'options_config.yaml')
+
+scraper_config = indeed_instance.scraper_config
+
+indeed_instance.run(
+    scraper_config['base_config']['job_titles']
+    , scraper_config['base_config']['number_of_pages'] 
+    )
+
+indeed_df = indeed_instance.output_to_dataframe() 
+
+indeed_df.to_csv('Indeed_Jobs.csv', index=False)
