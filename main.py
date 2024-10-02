5da15d26fed058bb7d25208d1e7589502b60bee4
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-02 21:54:21 +0100

    Moving config files to config directory and adjusting main.py to reflect changes

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from src.data_processing import DataProcessing
 from src.indeed_scraper import IndeedScraper
 from src.database_operations import DatabaseOperations
 
-indeed_instance = IndeedScraper("https://uk.indeed.com/", 'indeed_config.json', 'options_config.yaml')
+indeed_instance = IndeedScraper("https://uk.indeed.com/", 'config/indeed_config.json', 'config/options_config.yaml')
 
 scraper_config = indeed_instance.scraper_config
 
-indeed_instance.run(
-    scraper_config['base_config']['job_titles']
-    , scraper_config['base_config']['number_of_pages'] 
-    )
+def scrape_indeed():
+    indeed_instance.run(
+        scraper_config['base_config']['job_titles']
+        , scraper_config['base_config']['number_of_pages'] 
+        )
 
-indeed_df = indeed_instance.output_to_dataframe() 
+    indeed_df = indeed_instance.output_to_dataframe() 
 
-indeed_df.to_csv('Indeed_Jobs.csv', index=False)
+    indeed_df.to_csv('Indeed_Jobs.csv', index=False)
+    return indeed_df 
+
+if __name__ == "__main__":
+    scrape_indeed() 
