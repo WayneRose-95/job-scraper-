df76cc8c579ed15a99c5b27fe18ca1cce937b598
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-02 17:21:19 +0100

    Adding logic to select the correct selenium webdriver

--- src/general_scraper.py
+++ src/general_scraper.py
@@ -1,14 +1,37 @@
-import yaml 
+from fake_useragent import UserAgent
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ChromeService
+from selenium_stealth import stealth
+import undetected_chromedriver as uc
 import json 
+import yaml 
+
 
 class GeneralScraper:
     '''
     A class containing generic methods for webscraping 
 
     '''
-    def __init__(self, driver_config_file : str, file_type : str = 'yaml'):
+    def __init__(self, driver_config_file : str, file_type : str = 'yaml', website_options=False):
+        '''
+        Attributes
+
+        self.driver : takes in a new instance of the selenium webdriver object 
+
+        '''
         self.driver_config = self.load_driver_config(driver_config_file, file_type)
-        pass 
+        self.website_options = website_options 
+
+        self.driver_type = self.driver_config['driver_type']
+        if self.website_options:
+            self.options = self.select_options() 
+        
+        self.driver = self.select_driver() 
+
 
     def load_driver_config(self, config_path : str, file_type : str = 'json' or 'yaml' or 'yml'):
         '''
@@ -54,19 +77,180 @@ class GeneralScraper:
 
 
     def select_driver(self):
-        pass 
+        '''
+        Selects a driver based on what is passed into the driver_config.yaml file 
 
-    def select_options(self): 
-        pass 
+        Returns: 
+        self.driver : WebDriver 
+        A selenium WebDriver object
+        '''
+        if self.driver_type == 'stealth_driver':
+            self.driver = self.setup_stealth_driver()
+            return self.driver 
+        
+        elif self.driver_type == 'undetected_stealth_driver':
+            self.driver = self.setup_undetected_stealth_driver()
+            return self.driver
+        
+        elif self.driver_type == 'setup_driver':
+            self.driver = self.setup_driver()
+            return self.driver
 
-    def setup_driver(self):
-        pass 
+        else:
+            raise ValueError('Invalid driver selection only stealth_driver, undetected_stealth_driver and setup_driver are valid')
+
+    def select_options(self):
+        '''Selects options based on the driver selected from the driver_config.yaml file'''
+
+        if self.driver_type == 'undetected_stealth_driver':
+            options = uc.ChromeOptions()
     
-    def setup_stealth_driver(self):
-        pass 
+            for argument in self.driver_config['setup_driver']['arguments']:
+                options.add_argument(argument)
+            return options
+
+        elif self.driver_type == 'stealth_driver':
+            # Set up Selenium Chrome options
+            options = webdriver.ChromeOptions()
+            # Adding Arguments from the driver_config.yaml 
+            for argument in self.driver_config['setup_driver']['arguments']:
+                options.add_argument(argument)
+            # Adding experimental arguments from the driver_config.yaml
+            for option, argument in self.driver_config['setup_driver']['experimental_options'].items():
+                options.add_experimental_option(option, argument)
+
+            return options
+
+        elif self.driver_type == 'setup_driver':
+            options = Options()
+            # Adding Arguments from the driver_config.yaml 
+            for argument in self.driver_config['setup_driver']['arguments']:
+                options.add_argument(argument)
+
+            # Adding experimental arguments from the driver_config.yaml
+            for option, argument in self.driver_config['setup_driver']['experimental_options'].items():
+                options.add_experimental_option(option, argument)
+                # Break out of the loop to add only the first experimental option. 
+                break
+            return options 
 
     def setup_undetected_stealth_driver(self):
-        pass 
+        '''
+        Sets up an undetected stealth driver using the undetected-chrome package
+
+        Returns: 
+        driver : WebDriver 
+        A selenium WebDriver object
+        '''
+        
+        if self.website_options:
+            # Creating the undetected driver
+            driver = uc.Chrome(options=self.options)
+
+            # Select options from the driver_config.yaml file for selenium-stealth
+            selenium_stealth_options = self.driver_config['selenium-stealth']
+            # Apply selenium-stealth to further hide the selenium footprint
+            stealth(driver,
+                    languages=selenium_stealth_options['languages'],
+                    vendor=selenium_stealth_options['vendor'],
+                    platform=selenium_stealth_options['platform'],  
+                    webgl_vendor=selenium_stealth_options['webgl_vendor'],
+                    renderer=selenium_stealth_options['renderer'],
+                    fix_hairline=selenium_stealth_options['fix_hairline'],
+                    run_on_insecure_origins=selenium_stealth_options['run_on_insecure_origins']
+                    )
+        
+            return driver
+        
+        else:
+            # Creating the undetected driver without options
+            driver = uc.Chrome()
+
+            # Select options from the driver_config.yaml file for selenium-stealth
+            selenium_stealth_options = self.driver_config['selenium-stealth']
+            # Apply selenium-stealth to further hide the selenium footprint
+            stealth(driver,
+                    languages=selenium_stealth_options['languages'],
+                    vendor=selenium_stealth_options['vendor'],
+                    platform=selenium_stealth_options['platform'],  
+                    webgl_vendor=selenium_stealth_options['webgl_vendor'],
+                    renderer=selenium_stealth_options['renderer'],
+                    fix_hairline=selenium_stealth_options['fix_hairline'],
+                    run_on_insecure_origins=selenium_stealth_options['run_on_insecure_origins']
+                    )
+        
+            return driver
+
+        
+    def setup_driver(self):
+        """
+        Sets up the Selenium WebDriver with the appropriate Chrome options.
+
+        if the website_options argument is marked as False, then no ChromeOptions 
+        will be used. 
+
+        Returns: 
+        Chrome() : WebDriver
+
+        A selenium WebDriver object. 
+        """
+        if self.website_options:
+            ua = UserAgent(browsers=[self.driver_config['setup_driver']['browser']]) 
+            service = ChromeService(ChromeDriverManager().install())
+            return Chrome(service=service, options=self.options)
+        else:
+            ua = UserAgent(browsers=[self.driver_config['setup_driver']['browser']])
+            service = ChromeService(ChromeDriverManager().install())
+            # Setup driver without using the extra command line options
+            return Chrome(service=service)
+
+
+    
+    def setup_stealth_driver(self):
+        '''
+        Sets up an undetected stealth driver using the selenium-stealth package
+
+        Returns: 
+        driver : WebDriver 
+        A selenium WebDriver object
+        '''
+
+        if self.website_options:
+            driver = Chrome(
+                    service=Service(ChromeDriverManager().install()), options=self.options
+                )
+
+            # Set up selenium-stealth
+            selenium_stealth_options = self.driver_config['selenium-stealth']
+            stealth(driver,
+                    languages=selenium_stealth_options['languages'],
+                    vendor=selenium_stealth_options['vendor'],
+                    platform=selenium_stealth_options['platform'],  
+                    webgl_vendor=selenium_stealth_options['webgl_vendor'],
+                    renderer=selenium_stealth_options['renderer'],
+                    fix_hairline=selenium_stealth_options['fix_hairline']
+                    )
+
+            return driver
+        
+        else:
+            # Set up driver without using extra command line options
+            driver = Chrome(
+            service=Service(ChromeDriverManager().install())
+                )
+
+            # Set up selenium-stealth
+            selenium_stealth_options = self.driver_config['selenium-stealth']
+            stealth(driver,
+                    languages=selenium_stealth_options['languages'],
+                    vendor=selenium_stealth_options['vendor'],
+                    platform=selenium_stealth_options['platform'],  
+                    webgl_vendor=selenium_stealth_options['webgl_vendor'],
+                    renderer=selenium_stealth_options['renderer'],
+                    fix_hairline=selenium_stealth_options['fix_hairline']
+                    )
+
+            return driver
     
     def land_first_page(self):
         pass 
