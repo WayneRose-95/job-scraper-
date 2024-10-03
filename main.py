05803e5ae8ea203d0177fad6f79604ea37a75dd1
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-03 22:54:55 +0100

    adding create_job_database and process_dataframes methods

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ data_processor = S3DataProcessing('job-scraper-data-bucket')
 dataframe_manipulation = DataFrameManipulation() 
 operator = DatabaseOperations()
 scraper_config = indeed_instance.scraper_config
+target_db_config = operator.load_db_credentials('config/db_creds.yaml')
+database_name = target_db_config['DATABASE']
+print(database_name)
 
 def scrape_indeed():
     indeed_instance.run(
@@ -19,7 +22,7 @@ def scrape_indeed():
 
     indeed_df = indeed_instance.output_to_dataframe() 
 
-    indeed_df.to_csv('Indeed_Jobs.csv', index=False)
+    indeed_df.to_csv('indeed_jobs.csv', index=False)
     return indeed_df 
 
 def upload_to_s3(s3_file_name : str):
@@ -32,7 +35,70 @@ def upload_to_s3(s3_file_name : str):
 
     data_processor.upload_file_to_s3(s3_file_name, s3_object_name, file_directory)
 
+def create_job_database():
+    server_engine = operator.connect(target_db_config, new_db_name=database_name)
+    # Create the database
+    database = operator.create_database(server_engine, database_name)
+    # Once created create another engine to connect to the database itself. 
+    target_database_engine = operator.connect(target_db_config, connect_to_database=True, new_db_name=database_name)
+    return target_database_engine 
+    
+def process_dataframes(s3_file_path : str):
+    s3_objects = data_processor.list_objects(s3_file_path)
+    csv_files = data_processor.read_objects_from_s3(s3_objects, 'csv')
 
+    # Reading in a .csv from the s3 bucket
+    df = dataframe_manipulation.raw_to_dataframe(data_processor.list_of_objects)
+    # Creating the company_dimension table 
+    company_df = dataframe_manipulation.build_dimension_table(df, 'company_name', ["company_name_id", "company_name"])
+
+    # Creating the job_title dimension table 
+    job_title_df = dataframe_manipulation.build_dimension_table(df, 'job_title', ['job_title_id', 'job_title'])
+
+    # Creating the description dimension table
+    description_df = dataframe_manipulation.build_dimension_table(df, 'job_description', ['job_description_id', 'job_description'])
+
+    # Creating the job_url dimension table
+    job_url_df = dataframe_manipulation.build_dimension_table(df, 'job_url', ['job_url_id', 'job_url'])
+
+    # Creating the location dimension table 
+    location_df = dataframe_manipulation.build_dimension_table(df, 'location', ['location_id', 'location'])
+
+    # Creating the time dimension table
+    time_dimension_df = dataframe_manipulation.build_dimension_table(df, 'date_extracted', ['date_extracted_id', 'date_extracted'])
+
+    # Creating full time dimension table 
+
+    full_time_dimension_df = dataframe_manipulation.build_time_dimension_table(
+        time_dimension_df, 
+        'date_extracted', 
+        [
+            'date_extracted_id', 'date_uuid', 'year', 'month', 'day',
+            'date', 'timestamp', 'date_extracted','quarter', 'day_of_week',
+            'month_name', 'is_month_start', 'is_month_end', 'is_leap_year', 
+            'is_quarter_start', 'is_quarter_end'
+        ]
+        )
+    
+
+    # Building the fact table 
+    fact_table = dataframe_manipulation.build_fact_table(df, job_title_df, company_df, location_df, job_url_df, description_df, full_time_dimension_df)
+
+    dataframe_dict = {
+        "land_job_data" : df,
+        "dim_company" : company_df,
+        "dim_job_title": job_title_df, 
+        "dim_description": description_df, 
+        "dim_location": location_df,
+        "dim_date": full_time_dimension_df,
+        "dim_job_url": job_url_df,
+        "fact_job_data": fact_table
+    }
+    return dataframe_dict
 if __name__ == "__main__":
-    scrape_indeed() 
-    upload_to_s3('indeed_jobs.csv')
+    # scrape_indeed() 
+    # upload_to_s3('indeed_jobs.csv')
+    create_job_database() 
+    dataframe_dictionary = process_dataframes(f'indeed/{current_date.year}/{current_date.month}/{current_date.day}/')
+    land_job_data_table = dataframe_dictionary['land_job_data']
+    
