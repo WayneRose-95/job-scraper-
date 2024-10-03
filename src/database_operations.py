895e1b6401b25708b611c094cadf5821c8029ff4
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-03 02:14:10 +0100

    Adding create_database and connect methods

--- src/database_operations.py
+++ src/database_operations.py
@@ -1,13 +1,107 @@
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from sqlalchemy.engine import Engine
+from sqlalchemy import text
+import yaml 
 
 class DatabaseOperations: 
 
     def __init__(self):
         pass 
 
-    def load_db_credentials(self):
-        pass 
+    def load_db_credentials(self, config_path : str):
+        """
+        Method to load databae credentials from a .yaml file 
 
-    def connect(self):
+        Parameters: 
+
+        config_path : str 
+
+        The file path to the configuration file
+
+        Returns: 
+
+        config : dict 
+
+        A dictionary of database credentials 
+        """
+        try:
+            with open(config_path, 'r') as file:
+                config = yaml.safe_load(file)
+                print(type(config))
+            return config
+        except FileNotFoundError:
+            print(f"Configuration file {config_path} not found.")
+         
+
+    def connect(self, config_file : dict, connect_to_database=False, new_db_name=None):
+        DATABASE_TYPE = config_file['DATABASE_TYPE']
+        DBAPI = config_file['DBAPI']
+        USER = config_file['USER']
+        PASSWORD = config_file['PASSWORD']
+        ENDPOINT = config_file['ENDPOINT']
+        PORT = config_file['PORT']
+        DATABASE = config_file['DATABASE']
+
+        # If connecting to a database on the server
+        if connect_to_database:
+            if not new_db_name:
+                raise ValueError ("New database name not provided")
+            db_engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{new_db_name}?client_encoding=utf8", echo=True)
+            return db_engine
+
+        # Otherwise connect to the server directly. 
+        else:
+            db_engine_server = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}", echo=True)
+            return db_engine_server
+    
+    def list_db_tables(self, engine: Engine):
+        '''
+        Method to list the current database tables inside the database 
+
+        Parameters: 
+
+        engine : Engine 
+        A sqlalchemy Engine object. 
+
+        Returns: 
+
+        database_tables : list 
+
+        A list of strings which represent the list of tables within the database. 
+        '''
+        inspector = inspect(engine)
+        database_tables = inspector.get_table_names()
+
+        return database_tables
+
+    def create_database(self, engine : Engine, database_name : str):
+
+        with engine.connect() as database_engine:
+            if database_engine:
+                # If the database engine is present, then set the isolation level to autocommit 
+                database_engine.execution_options(isolation_level="AUTOCOMMIT")
+                # Call the database_name_check method to check if the database is present
+                result = self.database_name_check(engine, database_name)
+
+                database_exists = result.scalar() # Scalar result returns either 0 or 1
+                print(database_exists)
+                # If this statement is True, then the database already exists
+                if database_exists:
+                    print(f'{database_name} already exists, skipping creation')
+
+                else:
+                    print('Creating database')
+                    create_statement = text(f"CREATE DATABASE {database_name}")
+                    database_engine.execute(create_statement)
+                    print(f"Database {database_name} created successfully")
+            else:
+                print('Database creation failed')
+                raise Exception 
+
+    @staticmethod
+    def database_name_check():
+        
         pass 
 
     def read_rds_table(self):
@@ -16,15 +110,7 @@ class DatabaseOperations:
     def upsert_table(self):
         pass 
 
-    def list_db_tables(self):
-        pass 
 
-    def create_database(self):
-        pass 
-
-    @staticmethod
-    def database_name_check():
-        pass 
 
     def parse_column_type(self):
         pass 
