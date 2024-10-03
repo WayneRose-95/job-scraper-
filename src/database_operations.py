992a88b5fea3049fcae8a7debe2475f7efdbd0f7
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-03 14:36:28 +0100

    Adding docstrings to methods, adding extra methods

--- src/database_operations.py
+++ src/database_operations.py
@@ -168,36 +168,71 @@ class DatabaseOperations:
         pass 
 
 
+    
+    def parse_column_type(self, column_type : str):
+        """
+        Method to parse a column type from a string 
 
-    def parse_column_type(self, col_type : str):
+        Parameters: 
+
+        column_type : str 
+
+        The type of column in a string format 
+
+        Returns 
+        column_type_object : dict 
+
+        A dictionary which matches the column type to an integer 
+
+        """
         # Split the desired column name by the first occurence of the '(' character
-        # NOTE: the *col_params will set everything else that is split to a list. 
-        col_type_name, *col_params = col_type.split('(')
+        # NOTE: the *column_parameters will set everything else that is split to a list. 
+        column_type_name, *column_parameters = column_type.split('(')
         # Strip the Whitespace from the column_name 
-        col_type_name = col_type_name.strip()
-        # For the col_params variable select the first element of the list,
+        column_type_name = column_type_name.strip()
+        # For the column_parameters variable select the first element of the list,
         # then remove the last character from the list. 
         # Afterwards, apply the split method to split the rest of the string by ','s
-        # If the col_params variable is empty return an empty list
-        col_params = col_params[0][:-1].split(',') if col_params else []
+        # If the column_parameters variable is empty return an empty list
+        column_parameters = column_parameters[0][:-1].split(',') if column_parameters else []
         # Map the variable to a SQLAlchemy Type
-        col_type_obj = self.type_mapping[col_type_name]
-        # If condition for if col_params is not empty
-        if col_params:
-            # convert each value in the col_param variable to an integer
-            col_type_obj = col_type_obj(*map(int, col_params))
-        return col_type_obj
+        column_type_object = self.type_mapping[column_type_name]
+        # If condition for if column_parameters is not empty
+        if column_parameters:
+            # convert each value in the column_parameter variable to an integer
+            column_type_object = column_type_object(*map(int, column_parameters))
+        return column_type_object
 
     def generate_table_schema(self, table_name : str, columns : dict):
+        """
+        Method to generate the schema for a desired table name
+        Utilising SQLAlchemy's ORM syntax 
+
+        Parameters: 
+
+        table_name : str 
+
+        The name of the table. 
+
+        columns : dict 
+
+        A dictionary containing key-value pairs for each of the columns
+
+        Returns: 
+
+        table : Table 
+
+        A Table object representing the table to be created inside the database
+        """
         # Create a MetaData() object
         metadata = MetaData()
         table_columns = []
         # Iterate through the columns dictionary
-        for col_name, col_type in columns.items():
+        for column_name, column_type in columns.items():
             # In each iteration, call the parse_column_type function
-            col_type_obj = self.parse_column_type(col_type)
+            column_type_object = self.parse_column_type(column_type)
             # Append the tuple to the list of table_columns
-            table_columns.append(Column(col_name, col_type_obj))
+            table_columns.append(Column(column_name, column_type_object))
         # Create a Table object based on the metadata and schema of the columns dictionary
         table = Table(table_name, metadata, *table_columns)
 
@@ -209,6 +244,33 @@ class DatabaseOperations:
         return table
     
     def send_data_to_database(self, dataframe : DataFrame, engine : Engine, table_name : str, condition: str, schema_config : dict):
+        """
+        Method to send data to a database given a pandas DataFrame object 
+
+        Parameters: 
+
+        dataframe : DataFrame 
+
+        A pandas DataFrame object 
+
+        engine : Engine 
+
+        A sqlalchemy Engine object 
+
+        table_name : str 
+
+        The name of the table to be uploaded 
+
+        condition : str 
+
+        The table condition for the table to be uploaded. 
+        Options are 'append', 'replace' and 'fail' 
+
+        schema_config : dict 
+
+        A dictionary containing the configuration of the schema for the table
+        Found within a configuration file
+        """
         try:
             column_types = schema_config["schemas"]["tables"][table_name]
             table_schema = self.generate_table_schema(table_name, column_types)
@@ -221,6 +283,22 @@ class DatabaseOperations:
 
 
     def execute_sql(self, sql_file_path : str, engine : Engine):
+        '''
+        Method to execute a sql statement on a database given a sqlalchemy Engine object
+
+        Parameters: 
+
+        sql_file_path : str 
+
+        The file path to the sql script 
+
+        engine : Engine 
+
+        A sqlalchemy Engine object 
+
+        Returns: 
+        None 
+        '''
         sql_session = sessionmaker(bind=engine)
         session = sql_session()
 
