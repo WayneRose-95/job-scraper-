9e8e6518b2538f1642f39434d25e900d194baea8
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-10-17 16:13:43 +0100

    Adding process_page_links and extract totaljobs information methods to script

--- src/totaljobs_scraper.py
+++ src/totaljobs_scraper.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 from src.general_scraper import GeneralScraper
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep 
 from random import uniform 
 class TotalJobsScraper(GeneralScraper):
@@ -39,3 +43,53 @@ class TotalJobsScraper(GeneralScraper):
                 data[key] = self.extract_element(self.driver, value)
 
         return data
+    
+    def process_totaljobs_page_links(self):
+        list_of_urls = []
+
+        number_of_pages = self.scraper_config['base_config']['number_of_pages']
+
+        count = 0 
+        while count < number_of_pages:
+            try:
+                # Refetch job cards for each page iteration
+                list_of_job_cards = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located((By.XPATH, self.scraper_config['jobs']['start_extraction']['extract_data']['main_container']))
+                )
+                for index, job_card in enumerate(list_of_job_cards):
+                    try:
+                        url_element = job_card.find_element(By.XPATH, self.scraper_config['jobs']['start_extraction']['extract_data']['job_url'])
+                        url = url_element.get_attribute('href')
+                        list_of_urls.append(url)
+                        self.scroll_to_window(job_card)
+                        sleep(uniform(1, 5))
+                    except StaleElementReferenceException:
+                        sleep(2)
+                        self.driver.refresh() 
+                        url_element = WebDriverWait(self.driver, 5 )\
+                                    .until(EC.presence_of_element_located((By.XPATH, self.scraper_config['jobs']['start_extraction']['extract_data']['job_url'])))
+                        url = url_element.get_attribute('href')
+                        list_of_urls.append(url)
+                        self.scroll_to_window(job_card)
+                        sleep(uniform(1, 5))
+
+                sleep(2)
+                next_page = self.click_button_on_page(self.scraper_config['base_config']['next_page_xpath'])
+                if not next_page: 
+                    break 
+                count += 1
+            
+            except StaleElementReferenceException:
+                self.driver.refresh()  
+                sleep(3)
+
+        print(list_of_urls)
+        return list_of_urls 
+    
+    def extract_totaljobs_information(self, list_of_totaljobs_urls : list):
+        for url in list_of_totaljobs_urls:
+            sleep(uniform(2,4))
+            self.driver.get(url)
+            webpage_information = self.extract_job_details(self.scraper_config['jobs']['start_extraction']['extract_data'])
+            sleep(uniform(2, 5))
+            self.all_data_list.append(webpage_information)
